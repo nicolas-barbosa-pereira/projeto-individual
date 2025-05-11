d9a7042979985187428f4be0b2e79dd9496607ca
Nicolas Barbosa <nicolas.pereira009@sptech.school>
2025-05-11 13:49:14 -0300

    cadastrando no banco de dados na vm e modelagem de dados feita

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,65 +1,35 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database corinthians;
 
-/*
-comandos para mysql server
-*/
+use corinthians;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+create table usuario(
+idusuario int primary key auto_increment,
+nome varchar(100) not null,
+email varchar(100) not null,
+senha varchar(100) not null
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-    cpf char(11),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+create table quiz(
+idquiz int primary key auto_increment,
+tema varchar(100)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table pontuacao(
+idpontuacao int primary key auto_increment,
+fkquiz int not null,
+fkusuario int not null,
+pontos int,
+constraint fk_quiz foreign key(fkquiz) references quiz(idquiz),
+constraint fk_usuario foreign key(fkusuario) references usuario(idusuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+CREATE USER 'cori'@'%' IDENTIFIED BY 'Sptech#2025';
+CREATE USER 'cori_insert'@'%' IDENTIFIED BY 'SPtech#2025';
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+GRANT INSERT ON corinthians.* TO 'cori_insert'@'%';
+GRANT ALL privileges ON corinthians.* TO 'cori'@'%';
+
 
 select * from usuario;
