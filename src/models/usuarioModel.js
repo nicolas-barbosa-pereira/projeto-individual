7cea87171a9de9e209235700358dd536d6325622
Nicolas Barbosa <nicolas.pereira009@sptech.school>
2025-06-09 23:56:02 -0300

    arrumando detalhes

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -5,9 +5,8 @@ function jogadores() {
     console.log("acessei o user model na função jogadores");
 
     var instrucaoSql = `
-       SELECT
-        
-        jogadorFav, COUNT(*) AS votos
+       SELECT 
+       jogadorFav, COUNT(*) AS votos
 FROM usuario
 WHERE jogadorFav IS NOT NULL AND jogadorFav != ''
 GROUP BY jogadorFav
